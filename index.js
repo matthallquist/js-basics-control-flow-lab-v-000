de31502564fbfaa60a274bc5789b48b51d515b2c
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-25 18:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance >= 400) {
+    result = 0;
+  }
+  else {
+    result = distance;
+  }
+  return result;
+}
