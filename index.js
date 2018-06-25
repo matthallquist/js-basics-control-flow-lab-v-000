6963226cfa0e4110c2e4373025df65b8235c5f28
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-25 18:24:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!"
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    result = "No can do."
+  }
+  else {
+    result = distance;
+  }
+  return result;
+}
+
+function ternaryCheckCity(cityName) {
+  let result
+  if (cityName === "NYC") {
+    result = "Ok, sounds good."
+  }
+  else {
+    result = "No go."
+  }
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  let result
+  if (tip === "generous") {
+    result = "Thank you so much."
+  }
+  else if (tip === "not as generous") {
+    result = "Thank you."
+  }
+  else {
+    result = "Bye."
+  }
+  return result;
+}
